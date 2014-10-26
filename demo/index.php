fd9d627241827b06b387382138f374db6ad4aaf9
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2014-10-27 00:17:21 +0900

    use KnpPaginatorBundle

--- demo/index.php
+++ demo/index.php
@@ -1,7 +1,9 @@
 <?php
 use Quartet\Silex\Provider\PaginationServiceProvider;
 use Silex\Application;
+use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\TwigServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -10,20 +12,36 @@ $app->register(new TwigServiceProvider());
 $app['twig.path'] = array(__DIR__);
 $app->register(new PaginationServiceProvider());
 
-$app['debug'] = true;
+// just for demo.
+$app['knp_paginator.path'] = __DIR__ . '/../vendor/knplabs/knp-paginator-bundle';
 
-$app->get('/', function () use ($app) {
+// sample configuration.
+$app['knp_paginator.options'] = array(
+    'template' => array(
+        'pagination' => '@quartet_silex_pagination/pagination-bootstrap3.html.twig',
+    ),
+    'page_range' => 6,
+);
 
-    // sample data.
-    $sampleData = array();
-    for ($i = 1; $i <= 100; $i++) {
-        $sampleData[] = array(
-            'id' => $i,
-            'value' => sha1($i),
-        );
-    }
+// sample data.
+$app->register(new DoctrineServiceProvider(), [
+    'db.options' => [
+        'driver' => 'pdo_sqlite',
+        'path' => __DIR__ . '/sample.db',
+    ],
+]);
 
-    $pagination = $app['pagination']->paginate($sampleData);
+$app->get('/', function (Request $request) use ($app) {
+
+    $page = $request->get('page', 1);
+    $limit = $request->get('limit', 10);
+    $sort = $request->get('sort', 'id');
+    $direction = $request->get('direction', 'asc');
+
+    $sql = "select * from sample order by \"{$sort}\" {$direction}";
+    $array = $app['db']->fetchAll($sql);
+
+    $pagination = $app['knp_paginator']->paginate($array, $page, $limit);
 
     return $app['twig']->render('index.html.twig', array(
         'pagination' => $pagination,
--- src/Provider/PaginationServiceProvider.php
+++ src/Provider/PaginationServiceProvider.php
@@ -1,17 +1,25 @@
 <?php
 namespace Quartet\Silex\Provider;
 
-use Quartet\Silex\Service\PaginationService;
-use Quartet\Silex\Subscriber\PaginationSubscriber;
-use Quartet\Silex\Twig\Extension\PaginationExtension;
+use Knp\Bundle\PaginatorBundle\Helper\Processor;
+use Knp\Bundle\PaginatorBundle\Subscriber\SlidingPaginationSubscriber;
+use Knp\Bundle\PaginatorBundle\Twig\Extension\PaginationExtension;
+use Knp\Component\Pager\Event\Subscriber\Filtration\FiltrationSubscriber;
+use Knp\Component\Pager\Event\Subscriber\Paginate\PaginationSubscriber;
+use Knp\Component\Pager\Event\Subscriber\Sortable\SortableSubscriber;
+use Knp\Component\Pager\Paginator;
 use Silex\Application;
 use Silex\Provider\TranslationServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\ServiceProviderInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\Helper\RouterHelper;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class PaginationServiceProvider implements ServiceProviderInterface
 {
+    private $slidingPaginationSubscriber;
+
     /**
      * {@inheritdoc}
      */
@@ -27,28 +35,71 @@ class PaginationServiceProvider implements ServiceProviderInterface
             $app->register(new TranslationServiceProvider());
         }
 
-        $app['pagination.options'] = array();
+        // add twig extension.
+        $app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
+            $processor = new Processor(new RouterHelper($app['url_generator']), $app['translator']);
+            $twig->addExtension(new PaginationExtension($processor));
+            return $twig;
+        }));
 
-        $app['pagination'] = $app->share(function ($app) {
-            $app['pagination.options'] = array_replace(
+        $app['knp_paginator.path'] = __DIR__ . '/../../../../knplabs/knp-paginator-bundle';
+        $app['knp_paginator.limits'] = array(10, 25, 50, 100, 200, 500);
+        $app['knp_paginator.options'] = array();
+
+        // paginator creator.
+        $app['knp_paginator'] = $app->share(function ($app) {
+
+            // fix options.
+            $app['knp_paginator.options'] = array_replace_recursive(
                 array(
-                    'limits' => array(25, 50, 100, 200, 500),
-                ), $app['pagination.options']
+                    'default_options' => array(
+                        'sort_field_name' => 'sort',
+                        'sort_direction_name' => 'direction',
+                        'filter_field_name' => 'filterField',
+                        'filter_value_name' => 'filterValue',
+                        'page_name' => 'page',
+                        'distinct' => true,
+                    ),
+                    'template' => array(
+                        'pagination' => '@knp_paginator_bundle/sliding.html.twig',
+                        'filtration' => '@knp_paginator_bundle/filtration.html.twig',
+                        'sortable' => '@knp_paginator_bundle/sortable_link.html.twig',
+                    ),
+                    'page_range' => 5,
+                ), $app['knp_paginator.options']
             );
 
-            $app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
-                $twig->addExtension(new PaginationExtension($app));
-                return $twig;
-            }));
+            // add twig template paths.
             $loader = new \Twig_Loader_Filesystem();
             $loader->addPath(__DIR__ . '/../Views', 'quartet_silex_pagination');
+            $loader->addPath(rtrim($app['knp_paginator.path'], '/') . '/Knp/Bundle/PaginatorBundle/Resources/views/Pagination', 'knp_paginator_bundle');
             $app['twig.loader']->addLoader($loader);
 
-            return new PaginationService($app);
-        });
+            // add subscribers.
+            $app['dispatcher']->addSubscriber(new PaginationSubscriber());
+            $app['dispatcher']->addSubscriber(new SortableSubscriber());
+            $app['dispatcher']->addSubscriber(new FiltrationSubscriber());
+            $this->slidingPaginationSubscriber = new SlidingPaginationSubscriber(array(
+                'defaultPaginationTemplate' => $app['knp_paginator.options']['template']['pagination'],
+                'defaultSortableTemplate' => $app['knp_paginator.options']['template']['sortable'],
+                'defaultFiltrationTemplate' => $app['knp_paginator.options']['template']['filtration'],
+                'defaultPageRange' => $app['knp_paginator.options']['page_range'],
+            ));
+            $app['dispatcher']->addSubscriber($this->slidingPaginationSubscriber);
 
-        $app['pagination.subscriber'] = new PaginationSubscriber();
-        $app['dispatcher']->addSubscriber($app['pagination.subscriber']);
+            // create paginator.
+            $paginator = new Paginator($app['dispatcher']);
+            $paginator->setDefaultPaginatorOptions(array(
+                'pageParameterName' => $app['knp_paginator.options']['default_options']['page_name'],
+                'sortFieldParameterName' => $app['knp_paginator.options']['default_options']['sort_field_name'],
+                'sortDirectionParameterName' => $app['knp_paginator.options']['default_options']['sort_direction_name'],
+                'filterFieldParameterName' => $app['knp_paginator.options']['default_options']['filter_field_name'],
+                'filterValueParameterName' => $app['knp_paginator.options']['default_options']['filter_value_name'],
+                'distinct' => $app['knp_paginator.options']['default_options']['distinct'],
+            ));
+
+            return $paginator;
+        });
     }
 
     /**
@@ -56,5 +107,7 @@ class PaginationServiceProvider implements ServiceProviderInterface
      */
     public function boot(Application $app)
     {
+        $app['knp_paginator'];  // call registration to prepare "$this->slidingPaginationSubscriber" on ahead.
+        $app['dispatcher']->addListener(KernelEvents::REQUEST, array($this->slidingPaginationSubscriber, 'onKernelRequest'));
     }
 }
