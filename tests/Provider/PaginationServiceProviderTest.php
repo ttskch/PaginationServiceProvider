ba3dd122d90b372d55b4da21af33b6973c4a352c
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2014-11-09 19:44:25 +0900

    add unit test

--- tests/Provider/PaginationServiceProviderTest.php
+++ tests/Provider/PaginationServiceProviderTest.php
@@ -5,17 +5,42 @@ use Silex\Application;
 
 class PaginationServiceProviderTest extends \PHPUnit_Framework_TestCase
 {
-    private $app;
-
-    protected function setUp()
+    public function test_register()
     {
-        $this->app = new Application();
-        $this->app->register(new PaginationServiceProvider());
-    }
+        $app = new Application();
 
-    // todo
-    public function test()
-    {
-        $this->assertTrue(true);
+        $provider = new PaginationServiceProvider();
+        $provider->register($app);
+
+        // just for test.
+        $app['knp_paginator.path'] = __DIR__ . '/../../vendor/knplabs/knp-paginator-bundle';
+
+        // providers are registered.
+        $this->assertNotNull($app['twig']);
+        $this->assertNotNull($app['url_generator']);
+        $this->assertNotNull($app['translator']);
+
+        // TwigExtension is added.
+        $isExtensionRegistered = false;
+        $extensions = $app['twig']->getExtensions();
+        foreach (array_values($extensions) as $extension) {
+            if (get_class($extension) === 'Knp\Bundle\PaginatorBundle\Twig\Extension\PaginationExtension') {}
+            $isExtensionRegistered = true;
+        }
+        $this->assertTrue($isExtensionRegistered);
+
+        // can fix options.
+        $this->assertEmpty($app['knp_paginator.options']);
+        $app['knp_paginator.options_fixer'];
+        $this->assertNotEmpty($app['knp_paginator.options']);
+
+        // paginator is registered.
+        $this->assertInstanceOf('Knp\Component\Pager\Paginator', $app['knp_paginator']);
+
+        // subscribers are registered.
+        $this->assertInstanceOf('Knp\Component\Pager\Event\Subscriber\Paginate\PaginationSubscriber', $app['knp_paginator.pagination_subscriber']);
+        $this->assertInstanceOf('Knp\Component\Pager\Event\Subscriber\Sortable\SortableSubscriber', $app['knp_paginator.sortable_subscriber']);
+        $this->assertInstanceOf('Knp\Component\Pager\Event\Subscriber\Filtration\FiltrationSubscriber', $app['knp_paginator.filtration_subscriber']);
+        $this->assertInstanceOf('Knp\Bundle\PaginatorBundle\Subscriber\SlidingPaginationSubscriber', $app['knp_paginator.sliding_pagination_subscriber']);
     }
 }
