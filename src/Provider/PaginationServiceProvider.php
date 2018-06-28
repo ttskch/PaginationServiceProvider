ea295b4b97b09c5eadf8877f635a80fefc10bf71
Rubisel Prieto Dupeyrón <rubiselprieto@gmail.com>
2018-06-28 04:56:53 -0400

    Remove line by permit renplace default configuration. (#13)
    * Remove line by permit renplace default configuration.
    
    * Fix $app['knp_paginator.options'] undefine in php 5
    
    Define $app['knp_paginator.options'] for php5.

--- src/Provider/PaginationServiceProvider.php
+++ src/Provider/PaginationServiceProvider.php
@@ -36,10 +36,13 @@ class PaginationServiceProvider implements ServiceProviderInterface, BootablePro
 
             return $twig;
         });
+        
+        if(!isset($app['knp_paginator.options'])){
+            $app['knp_paginator.options'] = [];
+        }
 
         $app['knp_paginator.path'] = __DIR__ . '/../../../../knplabs/knp-paginator-bundle';
         $app['knp_paginator.limits'] = [10, 25, 50, 100, 200, 500];
-        $app['knp_paginator.options'] = [];
 
         // options fixer.
         $app['knp_paginator.options_fixer'] = function (Container $app) {
